caa259f4583332e59c42d47bed17ffe8e869f8f7
cats-will <60900416+cats-will@users.noreply.github.com>
2020-05-17 04:04:35 +0300

    Create circle.cpp

---
+++ С++/[4 laba] abstract class and virtual functions/circle.cpp
@@ -0,0 +1,53 @@
+#include "circle.h"
+
+const double pi = M_PI;
+
+
+
+Circle::Circle() {
+    this->Circle::initFromDialog();
+}
+void Circle::draw() {
+    cout << "Name: " << classname() << '\n';
+    cout << "Weight: " << mass() << '\n';
+    cout << "Radius: " << r << '\n';
+    cout << "Centre: " << position() << '\n';
+    cout << "Memory: " << size() <<'\n';
+}
+double Circle::mass() const {
+    return weight;
+}
+double Circle::square() {
+    return pi * pow(r, 2);
+}
+double Circle::perimeter() {
+    return 2 * pi * r;
+}
+unsigned int Circle::size() {
+    return sizeof(*this);
+}
+CVector2D Circle::position() const {
+    return centre;
+}
+void Circle::initFromDialog() {
+    cout << "      Enter centre's coords: ";
+    cin >> centre.x;
+    cin >> centre.y;
+    cout << "      Enter radius: ";
+    cin >> r;
+    cout << "      Enter weight: ";
+    cin >> weight;
+}
+const char* Circle::classname() const {
+    return name;
+}
+bool Circle::operator==(const IPhysObject& obj) const {
+    return mass() == obj.mass();
+}
+bool Circle::operator<(const IPhysObject& obj) const {
+    return mass() < obj.mass();
+}
+std::istream&operator>>(std::istream& istream, Circle& a) {
+    return istream >> a.centre.x >> a.centre.y >> a.r >> a.weight;
+}
+
