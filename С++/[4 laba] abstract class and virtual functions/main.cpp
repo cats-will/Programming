c70e6e168b9f440448caf2bbce1a2bb0605c2dd5
Kiti Ket <60900416+cats-will@users.noreply.github.com>
2020-05-18 04:03:00 +0300

    Update main.cpp

--- С++/[4 laba] abstract class and virtual functions/main.cpp
+++ С++/[4 laba] abstract class and virtual functions/main.cpp
@@ -1,12 +1,27 @@
 #include "circle.h"
 #include "ETriangle.h"
+#include <vector>
 
 int main() {
-    ETriangle a;
+    IFigure *a = new Circle();
+    std::vector<IFigure*> circles;
+    circles.push_back(a);
+    a->initFromDialog();
+    a->draw();
+    std::cout << "Circle's P = " << a->perimeter() << '\n';
+    std::cout << "Circle's S = " << a->square() << '\n' << std::endl;
 
-    a.initFromDialog();
-    a.draw();
-//    Circle b;
-//    b.initFromDialog();
-//    b.draw();
+    IFigure *b = new ETriangle;
+    std::vector<IFigure*> etriangles;
+    etriangles.push_back(b);
+    b->initFromDialog();
+    b->draw();
+    std::cout << "Etriangle's P = " << b->perimeter() << '\n';
+    std::cout << "Etriangle's S = " << b->square() << '\n';
+
+    bool q = a == b;
+    std::cout << "answer:" << q << ", test ==" << std::endl;
+
+    bool w = a < b;
+    std::cout << "answer:" << b << ", test <" << std::endl;
 }
