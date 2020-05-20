63d614958a228bafc829f6c4cd8d6c685ff9b53c
Kiti Ket <60900416+cats-will@users.noreply.github.com>
2020-05-20 10:16:07 +0300

    Update triangle.cpp

--- С++/[3 laba] son of the operator bitch/triangle.cpp
+++ С++/[3 laba] son of the operator bitch/triangle.cpp
@@ -6,6 +6,8 @@ Triangle::Triangle(Point x, Point y, Point z) : x_(x), y_(y), z_(z) {}
 
 Triangle::Triangle(const Triangle& a) : x_(a.x()), y_(a.y()), z_(a.z()) {}
 
+Triangle::~Triangle() {}
+
 Point Triangle::x() const {
     return this->x_;
 }
@@ -15,9 +17,6 @@ Point Triangle::y() const {
 Point Triangle::z() const {
     return this->z_;
 }
-
-Triangle::~Triangle() {}
-
 bool Triangle::operator==(Triangle &other) {
     return S() == other.S();
 }
